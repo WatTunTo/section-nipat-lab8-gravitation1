facfc9146be97705916646b004ed01a02c84def4
WatTunTo <nipat523naruto@gmail.com>
2025-03-10 16:35:33 +0700

    final

--- Assets/scripts/gravityB.cs
+++ Assets/scripts/gravityB.cs
@@ -1,14 +1,37 @@
 using System;
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class gravityB : MonoBehaviour
 {
     private Rigidbody rb;
+    const float G = 0.006674f;
+    public static List<gravityB> planetList;
+    
+    
+    private void FixedUpdate()
+    {
+        foreach (var planet in planetList)
+        {
+            if (planet != this)
+            {
+                attrack(planet);
+
+            }
+        }
+    }
 
     private void Awake()
     {
      rb = GetComponent<Rigidbody>();
+     if (planetList == null)
+     {
+         planetList = new List<gravityB>();
      }
+     planetList.Add(this);
+     
+    }
 
     void attrack(gravityB other)
     {
@@ -19,7 +42,10 @@ public class gravityB : MonoBehaviour
      float distance = direction.magnitude;
      
      // calculate gravity force
-     float forceMagnitude = 
+     float forceMagnitude = G * ((rb.mass * otherRb.mass) / (distance * distance));
+     Vector3 finalForce = forceMagnitude * direction.normalized;
+     
+     otherRb.AddForce(finalForce);
     }
     
     
